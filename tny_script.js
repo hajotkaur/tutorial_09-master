4b31beea9d669fc0cfa2ab91ffd28fe0f45e683e
hajotkaur <harjotkaur2113@gmail.com>
2024-10-09 11:38:55 -0600

    update

---
+++ tny_script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Todd Nash
+   Date: 2024-10-08
+
+*/
+
+/* Display date & time */
+
+document.getElementById("dateNow").innerHTML = `m/d/y/ <br /> h:m:s`;
